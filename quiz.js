6974e70463385ceb0ea3ccfd48aa3112060a0a5e
Step 25 <bhuvanadeepika017@gmail.com>
2019-12-08 20:48:06 +0530

    changed all globel variables
    What changes you have made?
    i changed all globel varibles

--- quiz.js
+++ quiz.js
@@ -3,55 +3,58 @@ const { getQuestions } = require("./getRandomQuestions.js");
 stdin.setRawMode(true);
 stdin.setEncoding("utf8");
 
-let questionsList = getQuestions();
-let score = 0;
-let questionNo = 0;
 let setTime;
 
-let displayQs = function(questionNo) {
-  let question = questionsList[questionNo];
-  stdout.write(`${question.question}\n`);
-  let options = question.options;
+let displayQs = function() {
+  let currentQuestion = this.questionsList[this.questionNo];
+  let options = currentQuestion.options;
+
+  stdout.write(`${this.questionNo + 1}.${currentQuestion.question}\n`);
   stdout.write(
     `a.${options.a}\nb.${options.b}\nc.${options.c}\nd.${options.d}\n`
   );
+
   setTime = setTimeout(() => {
     console.log("\n-------timeout-----");
-    giveNextQn();
-  }, 30000);
+    giveNextQn(this);
+  }, 15000);
 };
 
-const answerChecking = function(answer) {
-  if (answer == questionsList[questionNo].answer) {
-    score++;
+const answerChecking = function(answer, quizDetails) {
+  if (answer == quizDetails.questionsList[quizDetails.questionNo].answer) {
+    let score = ++quizDetails.score;
     console.log(`score is ${score}\nyour answer is correct`);
   } else {
-    console.log(`score is ${score}\nyour answer is wrong`);
+    console.log(`score is ${quizDetails.score}\nyour answer is wrong`);
   }
-  giveNextQn();
+
+  giveNextQn(quizDetails);
 };
 
-const giveNextQn = function() {
-  questionNo++;
-  if (questionNo < questionsList.length) {
-    displayQs(questionNo);
+const giveNextQn = function(quizDetails) {
+  let questionNo = ++quizDetails.questionNo;
+
+  if (questionNo < quizDetails.questionsList.length) {
+    displayQs.call(quizDetails);
   } else {
-    process.exit(0);
+    process.exit(quizDetails.score);
   }
 };
-
-stdin.on("data", UserInput => {
-  clearTimeout(setTime);
-  let answer = UserInput.trim();
-  answerChecking(answer);
-});
 
 const main = function() {
-  displayQs(questionNo);
+  let questionsList = getQuestions();
+  let quizDetails = { score: 0, questionNo: 0, questionsList };
+  displayQs.call(quizDetails);
+
+  stdin.on("data", UserInput => {
+    clearTimeout(setTime);
+    let answer = UserInput.trim();
+    answerChecking(answer, quizDetails);
+  });
 };
 
 main();
 
-process.on("exit", () =>
+process.on("exit", score =>
   console.log(`score is ${score}\n-------Quiz over------\n---- Thank You-----`)
 );
